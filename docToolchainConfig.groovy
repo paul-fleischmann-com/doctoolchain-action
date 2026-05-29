84db6580a0d76c39b3fdb841c5ecdf705d2d96c0
Paul Fleischmann <paul.fleischmann81@gmail.com>
2026-05-29 11:45:00 +0200

    update groovy config file

--- docToolchainConfig.groovy
+++ docToolchainConfig.groovy
@@ -3,8 +3,8 @@ inputPath = 'docs'
 outputPath = System.getenv('DTC_OUTPUT_PATH') ?: 'build/docs'
 
 inputFiles = [
-  [file: 'index.adoc', formats: ['html']],
-  [file: 'master-pdf.adoc', formats: ['pdf']],
+  [file: 'user-guide/index.adoc', formats: ['html']],
+  [file: 'user-guide/master-pdf.adoc', formats: ['pdf']],
 ]
 
 asciidoc = [
